f018ebf2c6117b7a2b4623305002353a6b58c327
Umesh Kumar <umeshkmr@gmail.com>
2024-04-30 00:59:57 +0300

    commit1

---
+++ Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+# import home, trending, account, your, about
+
+
+
+st.set_page_config(
+    page_title="TRANSACTION BASED ANALYTICS ",
+)
+
+st.title("Main Page")
+st.sidebar.success('Select a page above.')
+
+
