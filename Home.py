7405b35544e6122cb3de05709b4dca5e95d251f6
Umesh Kumar <umeshkmr@gmail.com>
2024-05-03 23:35:05 +0300

    v2.2

--- Home.py
+++ Home.py
@@ -8,7 +8,20 @@ st.set_page_config(
     page_title="TRANSACTION BASED ANALYTICS ",
 )
 
-st.title("Main Page")
+st.title("Data Analytics on PhonePe dataset")
 st.sidebar.success('Select a page above.')
 
+st.sidebar.image("images/umesh_profile_pic.jpg", use_column_width=True)
+
+
+'''
+#### PROJECT 2 
+'''
+
+
+'**Name:** R. Umesh Kumar'
+'**Batch Code:** MD108'
+'**Course:** Data Science Master Program by Guvi.in'
+'**Github link:** https://github.com/umeshkmrgithub/2_DS_ANALYTICS_APP.git '
+'**Email:** umeshkmr@gmail.com '
 
